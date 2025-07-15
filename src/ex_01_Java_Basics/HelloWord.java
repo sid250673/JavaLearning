f9b8e7355c02ed17090b74657c40d5c0f6184f9a
sid250673 <tester.siddharth@gmail.com>
2025-07-16 00:22:52 +0530

    Signed-off-by: sid250673 <tester.siddharth@gmail.com>

--- src/ex_01_Java_Basics/HelloWord.java
+++ src/ex_01_Java_Basics/HelloWord.java
@@ -3,6 +3,6 @@ package ex_01_Java_Basics;
 public class HelloWord {
 
     public static void main(String[] args) {
-        System.out.println("Print Hello World.");
+        System.out.println("Print Hello World");
     }
 }
